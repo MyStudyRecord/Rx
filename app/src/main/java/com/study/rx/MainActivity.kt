41186dc5cbc612eea83f4d4742457e0263772606
ParkSangSun <appleasz1028102@gmail.com>
2022-03-21 16:10:56 +0900

    fromIterable 오퍼레이터

--- app/src/main/java/com/study/rx/MainActivity.kt
+++ app/src/main/java/com/study/rx/MainActivity.kt
@@ -12,35 +12,13 @@ class MainActivity : AppCompatActivity() {
         const val TAG = "MainActivity"
     }
 
-    val mList = mutableListOf(1,2,3,4,5,6,7,8,9,10,11,12)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val observable = Observable.just(mList)
-
-        val observer = object : Observer<List<Int>> {
-            override fun onSubscribe(d: Disposable) {
-                Log.d(TAG, "onSubscribe")
-            }
-
-            override fun onError(e: Throwable) {
-                Log.d(TAG, "onError : $e")
-            }
-
-            override fun onComplete() {
-                Log.d(TAG, "onComplete")
-            }
-
-            override fun onNext(t: List<Int>) {
-                Log.d(TAG, "onNext : $t")
-            }
-
-        }
-
-        observable.subscribe(observer)
+        fromIterableOperator()
 
     }
 }
---
+++ app/src/main/java/com/study/rx/Operators.kt
@@ -0,0 +1,84 @@
+package com.study.rx
+
+import android.util.Log
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+import java.util.*
+
+val mListNum = mutableListOf(1,2,3,4,5,6,7,8,9,10,11,12)
+val arraysNum1 = arrayOf(1,2,3,4,5,6,7,8,9,10,11,12)
+val arraysNum2 = arrayOf(10,20,30,40,50,60,70,80,90,100,110,120)
+
+fun justOperator(){
+    val observable = Observable.just(mListNum)
+
+    val observer = object : Observer<List<Int>> {
+        override fun onSubscribe(d: Disposable) {
+            Log.d(MainActivity.TAG, "onSubscribe")
+        }
+
+        override fun onError(e: Throwable) {
+            Log.d(MainActivity.TAG, "onError : $e")
+        }
+
+        override fun onComplete() {
+            Log.d(MainActivity.TAG, "onComplete")
+        }
+
+        override fun onNext(t: List<Int>) {
+            Log.d(MainActivity.TAG, "onNext : $t")
+        }
+
+    }
+
+    observable.subscribe(observer)
+}
+
+fun fromOperator(){
+    val observable = Observable.fromArray(arraysNum1)
+
+    val observer = object : Observer<Array<Int>> {
+        override fun onSubscribe(d: Disposable) {
+            Log.d(MainActivity.TAG, "onSubscribe")
+        }
+
+        override fun onError(e: Throwable) {
+            Log.d(MainActivity.TAG, "onError : $e")
+        }
+
+        override fun onComplete() {
+            Log.d(MainActivity.TAG, "onComplete")
+        }
+
+        override fun onNext(t: Array<Int>) {
+            Log.d(MainActivity.TAG, "onNext : ${Arrays.toString(t)}")
+        }
+    }
+
+    observable.subscribe(observer)
+}
+
+fun fromIterableOperator(){
+    val observable = Observable.fromIterable(mListNum)
+
+    val observer = object : Observer<Int> {
+        override fun onSubscribe(d: Disposable) {
+            Log.d(MainActivity.TAG, "onSubscribe")
+        }
+
+        override fun onError(e: Throwable) {
+            Log.d(MainActivity.TAG, "onError : $e")
+        }
+
+        override fun onComplete() {
+            Log.d(MainActivity.TAG, "onComplete")
+        }
+
+        override fun onNext(t: Int) {
+            Log.d(MainActivity.TAG, "onNext : $t")
+        }
+    }
+
+    observable.subscribe(observer)
+}
